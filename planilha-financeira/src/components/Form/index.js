d424566e86bce5af4e0ef47eda559ecbcaf65480
Paulo Turino <paulo.turino01@gmail.com>
2023-06-01 14:55:25 -0300

    Enviando dados do form para o back-end

--- planilha-financeira/src/components/Form/index.js
+++ planilha-financeira/src/components/Form/index.js
@@ -1,14 +1,15 @@
 import styles from './Form.module.css';
 import React, { useState } from 'react';
 
-export default function Form() {
+export default function Form({ despesasAnteriores, receitasAnteriores, valoresAnteriores}) {
 
     const [showFormReceita, setShowFormReceita] = useState(true)
     const [showFormDespesa, setShowFormDespesa] = useState(false)
 
     const [nomeReceita, setNomeReceita] = useState()
     const [nomeDespesa, setNomeDespesa] = useState()
-    const [valor, setValor] = useState()
+    const [valorReceita, setValorReceita] = useState()
+    const [valorDespesa, setValorDespesa] = useState()
     
     function mostrarFormDespesa(e) {
         e.preventDefault();
@@ -22,15 +23,44 @@ export default function Form() {
         setShowFormDespesa(false);
     }
 
-    function adicionaNaPlanilha(e) {
-        e.preventDefault();
-        console.log(nomeReceita)
-        console.log(nomeDespesa);
-        console.log(valor)
+    function submitDespesa(e) {
+        e.preventDefault()
+        let novaDespesa = {
+            nome: nomeDespesa,
+            valor: valorDespesa
+        }
+        fetch('http://localhost:5000/despesas', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(novaDespesa),
+        })
+            .then((r) => r.json())
+            .then((data) => console.log(data))
+            .catch(err => console.log(err))
+    }
+
+    function submitReceita(e) {
+        e.preventDefault()
+        let novaReceita = {
+            nome: nomeReceita,
+            valor: valorReceita
+        }
+        fetch('http://localhost:5000/receitas', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(novaReceita),
+        })
+            .then((r) => r.json())
+            .then((data) => console.log(data))
+            .catch(err => console.log(err))
     }
 
     return(
-        <form onSubmit={adicionaNaPlanilha} className={styles.form}>
+        <div className={styles.form}>
             <div className={styles.botoes}>
                 <button onClick={mostrarFormReceita}>Adicionar receita</button>
                 <button onClick={mostrarFormDespesa}>Adicionar despesa</button>
@@ -38,41 +68,69 @@ export default function Form() {
 
             {
                 showFormDespesa && !showFormReceita ? 
-                    <div className={styles.insideForm}>
+                    <form onSubmit={submitDespesa} className={styles.insideForm}>
                         
                         <div className={styles.campo}>
                             <label htmlFor='despesa'>Despesa:</label>
-                            <input onChange={(e) => setNomeDespesa(e.target.value)} type='text' name='despesa' id='despesa' placeholder='Nome da despesa...' />
+                            <input 
+                                required
+                                type='text' 
+                                name='despesa' 
+                                id='despesa' 
+                                placeholder='Nome da despesa...'
+                                onChange={(e) => setNomeDespesa(e.target.value)} 
+                            />
                         </div>
 
                         <div className={styles.campo}>
                             <label htmlFor='valor'>Valor:</label>
-                            <input onChange={(e) => setValor(e.target.value)} type='number' name='valor' id='valor' placeholder='Valor da despesa...' />
+                            <input
+                                required 
+                                type='number' 
+                                name='valor' 
+                                id='valor' 
+                                placeholder='Valor da despesa...'
+                                onChange={(e) => setValorDespesa(e.target.value)}  
+                            />
                         </div>
 
                         <input className={styles.btnr} type='submit' value='Adicionar despesa' />
-                    </div> 
+                    </form> 
                 : null
             }
                         
             {
                 showFormReceita && !showFormDespesa ? 
-                    <div className={styles.insideForm}>
+                    <form onSubmit={submitReceita} className={styles.insideForm}>
                         
                         <div className={styles.campo}>
                             <label htmlFor='receita'>Receita:</label>
-                            <input  onChange={(e) => setNomeReceita(e.target.value)} type='text' name='receita' id='receita' placeholder='Nome da receita...' />
+                            <input 
+                                required
+                                type='text' 
+                                name='receita' 
+                                id='receita' 
+                                placeholder='Nome da receita...'
+                                onChange={(e) => setNomeReceita(e.target.value)}  
+                            />
                         </div>
 
                         <div className={styles.campo}>
                             <label htmlFor='valor'>Valor:</label>
-                            <input onChange={(e) => setValor(e.target.value)} type='number' name='valor' id='valor' placeholder='Valor da receita...' />
+                            <input 
+                                required
+                                type='number' 
+                                name='valor' 
+                                id='valor' 
+                                placeholder='Valor da receita...' 
+                                onChange={(e) => setValorReceita(e.target.value)}
+                            />
                         </div>
 
                         <input className={styles.btng} type='submit' value='Adicionar receita' />
-                    </div>
+                    </form>
                 : null
             }
-        </form>
+        </div>
     )
 }
